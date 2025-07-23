2649dda75268735cd40ec236fd3a748c3de451e2
Dev Pereira <47191733+DevPereira1st@users.noreply.github.com>
2025-07-23 21:13:03 +0000

    Não sei se consegui

--- CartasSuperTrunfo em C/CartasSuperTrunfo.c
+++ CartasSuperTrunfo em C/CartasSuperTrunfo.c
@@ -2,9 +2,9 @@
 
 int main() {
 // Cadastro de Cartas
-    char estado;
-    char codigo[4];
-    char idade[10];
+    char estado[50];
+    char codigo[50];
+    char cidade[50];
     int populacao;
     float area;
     float pib;
@@ -15,16 +15,16 @@ int main() {
     printf("Selecione uma letra de A a H:");
     scanf("%s", &estado);
 
-    printf("Digite o codigo da Cidade. Ex: D001: ");
+    printf("Digite o codigo da Cidade: ");
     scanf("%s", &codigo);
 
-    printf("Digite o nome da Cidade. Ex: Brasilia: ");
+    printf("Digite o nome da Cidade: ");
     scanf("%s", &cidade);
 
     printf("Digite o total de moradores na cidade: ");
     scanf("%d", &populacao);
 
-    printf("Digite a área (tamanho) da cidade. Ex: 1000 (km²): ");
+    printf("Digite a área (tamanho) da cidade: ");
     scanf("%f", &area);
 
     printf("Digite digite qual o PIB da cidade: ");
@@ -33,15 +33,17 @@ int main() {
     printf("Quantos pontos turísticos a cidade possui?: ");
     scanf("%d", &ptr);
 
+    // Chat Log da Carta
+
     printf("✦✦Carta Cadastrada com Sucesso!!✦✦\n\n ┌┌Informações da Carta:┐┐ \n\n");
 
-    printf("Código do Estado: %s \n", estado);
-    printf("Código: %s \n", codigo);
-    printf("Nome da Cidade: %s \n", cidade);
-    printf("População: %d \n", populacao);
-    printf("Área: %.1f km² \n", area);
-    printf("PIB: %.1f \n", pib);
-    printf("Total pontos turísticos: %d \n", ptr);
+    printf("Letra do estado: %s\n",estado);
+    printf("Código: %s\n",codigo);
+    printf("Nome da Cidade: %s\n",cidade);
+    printf("População: %d \n",populacao);
+    printf("Área: %.1f km² \n",area);
+    printf("PIB: %.1f \n",pib);
+    printf("Total pontos turísticos: %d \n",ptr);
 
     return 0;
 }
